ffe64d61a675842b8d64b5cf43d5cfd201615a36
Nahuel-a <nahuel.94.arrieta@gmail.com>
2025-10-06 09:58:46 -0300

    feat: Implement user retrieval by ID with error handling

--- backend/app/api/routers/user.py
+++ backend/app/api/routers/user.py
@@ -1,4 +1,5 @@
-#from api.dependencies.auth import validate_authenticate_user
+from api.dependencies.auth import validate_authenticate_user
+from sqlalchemy.exc import SQLAlchemyError
 from api.dependencies.db import get_session
 from crud.user import UserCrud
 from fastapi import APIRouter, Depends, HTTPException, status
@@ -32,3 +33,36 @@ async def create_user(
     user_create.password = hashed_password
     new_user = await UserCrud(db).create(user_create)
     return new_user
+
+@router.get(
+    "/{user_id}/",
+    status_code=status.HTTP_200_OK,
+    response_model=UserSchema,
+)
+async def get_user_id(
+    user_id: int,
+    db: AsyncSession = Depends(get_session),
+    curret_user: str = Depends(validate_authenticate_user),
+):
+    """
+    Retrieves a user by its ID. Checks that the user exists in the database.
+    """
+    try:
+        user = await UserCrud(db).get(user_id)
+        if user is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"User with id {user_id} not exist",
+            )
+        return user
+
+    except ValueError as ve:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(ve),
+        )
+    except SQLAlchemyError as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Database error occurred",
+        )
