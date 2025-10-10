f7d42a61a1b3c7861671013f7370dd8d55994096
Nahuel-a <nahuel.94.arrieta@gmail.com>
2025-10-10 17:26:27 -0300

    refactor: Clean up comments and improve code readability in login function

--- backend/app/api/routers/auth.py
+++ backend/app/api/routers/auth.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 from api.dependencies.db import get_session
 from core.config import get_settings
-from fastapi import APIRouter, Depends, HTTPException #Se agrega para manejar el error si el usuario no esta en la base de datos
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from schemas.user import Token, UserLogin
 from sqlalchemy.ext.asyncio.session import AsyncSession
@@ -23,13 +23,12 @@ async def login(
     # Verify user credentials in the database.
     db_user = await is_authenticate(user.username, user.password, db)
     
-    #Si la autenticacion falla, no se rompe y tira error
     if not db_user:
         raise HTTPException(status_code=401, detail="Invalid credentials")
      
     access_token_expire = timedelta(minutes=int(settings.EXPIRE_TOKEN))
     # Create an access token for the authenticated user with a defined expiration.
-    access_token = await oauth2.create_access_token(#estaba escrito create_acces_token
+    access_token = await oauth2.create_access_token(
         data={"email": db_user.email}, expires_delta=access_token_expire
     )
     return Token(access_token=access_token, token_type="bearer")
